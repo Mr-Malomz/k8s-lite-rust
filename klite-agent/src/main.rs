b1914b4e90fb7c2418c74689dcbe8bf2cf79b533
demola malomo <malomo.alaba@yahoo.com>
2026-09-08 01:03:26 +0100

    feat: implement polling loop in klite-agent to periodically fetch and display pod data via reqwest

--- klite-agent/src/main.rs
+++ klite-agent/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+
+async fn fetch_pods(client: &reqwest::Client) -> anyhow::Result<Vec<klite_core::Pod>> {
+    let pods = client
+        .get("http://127.0.0.1:3000/pods")
+        .send()
+        .await?
+        .json::<Vec<klite_core::Pod>>()
+        .await?;
+
+    Ok(pods)
+}
+
+
+#[tokio::main]
+async fn main() {
+    let client = reqwest::Client::new();
+
+    loop {
+        match fetch_pods(&client).await {
+            Ok(pods) => println!("{:#?}", pods),
+            Err(e) => eprintln!("Failed to fetch pods: {e}"),
+        }
+        tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+    }
 }
